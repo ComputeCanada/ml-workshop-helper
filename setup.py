df97d8e80edd8983d7312377ac9d3aec21b354f5
Evgeny Naumov <evgeny.naumov@calculquebec.ca>
2019-04-03 16:33:06 -0400

    attempting to include data files

--- setup.py
+++ setup.py
@@ -2,12 +2,19 @@ import numpy.distutils.misc_util as ndist_misc
 from setuptools import Extension, setup
 
 # semver with automatic minor bumps keyed to unix time
-__version__ = '1.0.1554323447'
-
+__version__ = '1.0.1555089001'
 
 setup(
     name="ml_helper",
     version=__version__,
     packages=["ml_helper"],
+    data_files=[
+        (
+            'data', [
+                'ml_helper/data/conductors_test.csv.gz',
+                'ml_helper/data/conductors_train.csv.gz'
+            ]
+        ),
+    ],
     include_package_data=True,
 )
